bca52c15eafacd3fe17d642d8c8dba4b02384bd1
Ruben Ariel Villalobos <ariel.villalobos96@gmail.com>
2020-10-25 20:50:30 -0300

    1-Creacion de un nuevo proyecto con composer y PHPUnit

---
+++ tests/AccessHandlerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Styde\AccessHandler as Access;
+class AccessHandlerTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_grant_access()
+    {
+        $this->assertTrue(
+            Access::check('admin')
+        );
+    }
+
+    public function test_deny_access()
+    {
+        $this->assertFalse(Access::check('sub-admin'));
+    }
+
+}
